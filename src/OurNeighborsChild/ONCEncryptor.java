0a591c2e3ce0b33173a7b437d38c57a7574b1ae1
oneilljw <johnwoneill1@gmail.com>
2015-06-08 15:54:48 -0400

    updates

--- src/OurNeighborsChild/ONCEncryptor.java
+++ src/OurNeighborsChild/ONCEncryptor.java
@@ -1,6 +1,5 @@
 package OurNeighborsChild;
 
-import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -8,55 +7,8 @@ import org.apache.commons.codec.binary.Base64;
 
 public class ONCEncryptor
 {
-	private static String key = "ONC12/25ONC12/25"; // 128 bit key
 	private static String secretKey = "XMzDdG4D03CKm2IxIWQw7g==";
 
-/*
-	public static String encrypt(String text)
-	{
-		byte[] encrypted = null;
-		
-		try
-		{
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-
-			// encrypt the text
-			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-			encrypted = cipher.doFinal(text.getBytes());
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-
-		return new String(encrypted);
-	}
-	
-	public static String decrypt(String encryptedText)
-	{
-		byte[] encryptedBytes = encryptedText.getBytes();
-		String decrypted = null;
-		
-		try
-		{
-			Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-
-			// encrypt the text
-			cipher.init(Cipher.DECRYPT_MODE, aesKey);
-			decrypted = new String(cipher.doFinal(encryptedBytes));
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-
-		return decrypted;
-	}
-*/	
 	public static String encrypt(String text)
 	{
         byte[] raw;
