2fb260c7892b8f7b219eecaad048dd1ff9f20d1b
oneilljw <johnwoneill1@gmail.com>
2015-07-15 15:36:03 -0400

    update to ONCWebsiteFamily

--- src/ourneighborschild/ONCWebsiteFamily.java
+++ src/ourneighborschild/ONCWebsiteFamily.java
@@ -10,130 +10,41 @@ public class ONCWebsiteFamily
 	private String	HOHFirstName;
 	private String	HOHLastName;	
 	private String	mealStatus;
+	private int		agentID;
 	
 	public ONCWebsiteFamily(ONCFamily f)
 	{
 		String[] famstatus = {"Unverified", "Info Verified", "Gifts Selected", "Gifts Received", "Gifts Verified", "Packaged"};
 		String[] delstatus = {"Empty", "Contacted", "Confirmed", "Assigned", "Attempted", "Returned", "Delivered", "Counselor Pick-Up"};
-		id = f.id;
-		oncNum = f.getONCNum();
-		fstatus = famstatus[f.getFamilyStatus()];
-		fstatus = delstatus[f.getDeliveryStatus()];
-		DNSCode = f.getDNSCode();
-		HOHFirstName = f.getHOHFirstName();
-		HOHLastName = f.getHOHLastName();
-		mealStatus = f.getMealStatus().toString();
+		this.id = f.id;
+		this.oncNum = f.getONCNum();
+		this.fstatus = famstatus[f.getFamilyStatus()];
+		this.dstatus = delstatus[f.getDeliveryStatus()];
+		this.DNSCode = f.getDNSCode();
+		this.HOHFirstName = f.getHOHFirstName();
+		this.HOHLastName = f.getHOHLastName();
+		this.mealStatus = f.getMealStatus().toString();
+		this.agentID = f.getAgentID();
 	}
 
-	/**
-	 * @return the id
-	 */
-	int getId() {
-		return id;
-	}
+	
+	int getId() { return id; }
+	String getOncNum() { return oncNum; }
+	String getFstatus() { return fstatus; }
+	String getDstatus() { return dstatus; }
+	String getDNSCode() { return DNSCode; }
+	String getHOHFirstName() {return HOHFirstName;}
+	String getHOHLastName() { return HOHLastName; }
+	String getMealStatus() { return mealStatus; }
+	int getAgentID() { return agentID; }
 
-	/**
-	 * @return the oncNum
-	 */
-	String getOncNum() {
-		return oncNum;
-	}
-
-	/**
-	 * @return the fstatus
-	 */
-	String getFstatus() {
-		return fstatus;
-	}
-
-	/**
-	 * @return the dstatus
-	 */
-	String getDstatus() {
-		return dstatus;
-	}
-
-	/**
-	 * @return the dNSCode
-	 */
-	String getDNSCode() {
-		return DNSCode;
-	}
-
-	/**
-	 * @return the hOHFirstName
-	 */
-	String getHOHFirstName() {
-		return HOHFirstName;
-	}
-
-	/**
-	 * @return the hOHLastName
-	 */
-	String getHOHLastName() {
-		return HOHLastName;
-	}
-
-	/**
-	 * @return the mealStatus
-	 */
-	String getMealStatus() {
-		return mealStatus;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @param oncNum the oncNum to set
-	 */
-	void setOncNum(String oncNum) {
-		this.oncNum = oncNum;
-	}
-
-	/**
-	 * @param fstatus the fstatus to set
-	 */
-	void setFstatus(String fstatus) {
-		this.fstatus = fstatus;
-	}
-
-	/**
-	 * @param dstatus the dstatus to set
-	 */
-	void setDstatus(String dstatus) {
-		this.dstatus = dstatus;
-	}
-
-	/**
-	 * @param dNSCode the dNSCode to set
-	 */
-	void setDNSCode(String dNSCode) {
-		DNSCode = dNSCode;
-	}
-
-	/**
-	 * @param hOHFirstName the hOHFirstName to set
-	 */
-	void setHOHFirstName(String hOHFirstName) {
-		HOHFirstName = hOHFirstName;
-	}
-
-	/**
-	 * @param hOHLastName the hOHLastName to set
-	 */
-	void setHOHLastName(String hOHLastName) {
-		HOHLastName = hOHLastName;
-	}
-
-	/**
-	 * @param mealStatus the mealStatus to set
-	 */
-	void setMealStatus(String mealStatus) {
-		this.mealStatus = mealStatus;
-	}
+	void setId(int id) { this.id = id; }
+	void setOncNum(String oncNum) { this.oncNum = oncNum; }
+	void setFstatus(String fstatus) { this.fstatus = fstatus; }
+	void setDstatus(String dstatus) { this.dstatus = dstatus; }
+	void setDNSCode(String dNSCode) { this.DNSCode = dNSCode; }
+	void setHOHFirstName(String hOHFirstName) {this.HOHFirstName = hOHFirstName; }	
+	void setHOHLastName(String hOHLastName) { this.HOHLastName = hOHLastName; }	
+	void setMealStatus(String mealStatus) {this.mealStatus = mealStatus; }
+	void setAgentID(int agtID) {this.agentID = agtID; }
 }
