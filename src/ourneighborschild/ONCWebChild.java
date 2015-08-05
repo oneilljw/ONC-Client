5396ec4aef937e943bfdc5386c995b658b950318
oneilljw <johnwoneill1@gmail.com>
2015-08-05 09:37:02 -0400

    Added ONCWebChild to interface with web clients

---
+++ src/ourneighborschild/ONCWebChild.java
@@ -0,0 +1,30 @@
+package ourneighborschild;
+
+
+public class ONCWebChild
+{
+	private int			famid;
+	private String		firstname;
+	private String		lastname;
+	private String		school;
+	private String		gender;
+	private String		sDOB;
+	
+	public ONCWebChild(ONCChild c)
+	{
+		this.famid = c.getFamID();
+		this.firstname = c.getChildFirstName();
+		this.lastname = c.getChildLastName();
+		this.school = c.getChildSchool();
+		this.gender = c.getChildGender();
+		this.sDOB = c.getChildDOBString("M/dd/yyyy");
+	}
+	
+	//getters
+	int getFamID() { return famid; }
+	String getFirstName() { return firstname; }
+	String getLirstName() { return lastname; }
+	String getSchool() { return school; }
+	String getGender() { return gender; }
+	String getDOB() { return sDOB; }
+}
