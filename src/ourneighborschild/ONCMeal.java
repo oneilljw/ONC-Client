82a1f2833fa4c32a2da86b3c00975d239ce8c7d8
oneilljw <johnwoneill1@gmail.com>
2015-08-10 10:56:57 -0400

    update to ONCMeal for agent website

--- src/ourneighborschild/ONCMeal.java
+++ src/ourneighborschild/ONCMeal.java
@@ -57,6 +57,7 @@ public class ONCMeal extends ONCEntity
 		row.add(Integer.toString(id));
 		row.add(Integer.toString(famID));		
 		row.add(type.toString());
+		row.add(Integer.toString(partnerID));
 		row.add(dietaryRestrictions);
 		row.add(changedBy);
 		row.add(Long.toString(dateChanged.getTimeInMillis()));
