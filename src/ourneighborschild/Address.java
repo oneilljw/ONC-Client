a892e43d5777d73d4099b02a907f7e5ffa0bfd1c
oneilljw <johnwoneill1@gmail.com>
2016-02-18 18:02:48 -0500

    update to continue Address object definition

--- src/ourneighborschild/Address.java
+++ src/ourneighborschild/Address.java
@@ -2,30 +2,63 @@ package ourneighborschild;
 
 public class Address 
 {
-	private String streetnum;
-	private String streetname;
+	private String streetNum;
+	private String streetDir;
+	private String streetName;
+	private String streetType;
+	private String streetPostDir;
 	private String unit;
 	private String city;
 	private String zip;
+	private String region;
 	
 	public Address(String streetnum, String streetname, String unit, String city, String zip)
 	{
-		this.streetnum = streetnum;
-		this.streetname = streetname;
+		this.streetNum = streetnum;
+		this.streetName = streetname;
 		this.unit = unit;
 		this.city = city;
 		this.zip = zip;
 	}
 	
-	public String getStreetnum() { return streetnum; }
-	public String getStreetname() { return streetname; }
+	public Address(String[] parts)
+	{
+		if(parts.length == 9)
+		{
+			this.streetNum = parts[0];
+			this.streetDir = parts[1];
+			this.streetName = parts[2];
+			this.streetType = parts[3];
+			this.streetPostDir = parts[4];
+			this.unit = parts[5];
+			this.city = parts[6];
+			this.zip = parts[7];
+			this.region = parts[8];
+		}
+		else
+		{
+			
+		}
+	}
+	
+	public String getStreetNum() { return streetNum; }
+	public String getStreetDir() { return streetDir; }
+	public String getStreetName() { return streetName; }
+	public String getStreetType() { return streetType; }
+	public String getStreetPostDir() { return streetPostDir; }
 	public String getUnit() { return unit; }
 	public String getCity() { return city; }
 	public String getZipCode() { return zip; }
+	public String getRegion() { return region; }
 	
-	public void setStreetnum(String s) { streetnum = s; }
-	public void setStreetname(String s) { streetname = s; }
+	
+	public void setStreetNum(String s) { streetNum = s; }
+	public void setStreetDir(String s) { streetDir = s; }
+	public void setStreetName(String s) { streetName = s; }
+	public void setStreetType(String s) { streetType = s; }
+	public void setStreetPostDir(String s) { streetPostDir = s; }
 	public void setUnit(String s) { unit = s; }
 	public void setCity(String s) { city = s; }
 	public void setZipCode(String s) { zip = s; }
+	public void setRegion(String s) { region = s; }
 }
