7adc6138c9a0659a7f47b8866a3c698c354ba421
oneilljw <johnwoneill1@gmail.com>
2016-05-24 23:48:22 -0400

    update to database event processing and repaint

--- src/ourneighborschild/WishLabelViewer.java
+++ src/ourneighborschild/WishLabelViewer.java
@@ -46,10 +46,7 @@ public class WishLabelViewer extends JDialog implements DatabaseListener
 		if(childWishDB != null)
 			childWishDB.addDatabaseListener(this);
 
-//		this.getContentPane().setBackground(Color.WHITE);
-		
 		labelPanel = new WishLabelPanel();
-		
 		img = GlobalVariables.getSeasonIcon().getImage();
 		
 		this.setContentPane(labelPanel);
@@ -71,9 +68,8 @@ public class WishLabelViewer extends JDialog implements DatabaseListener
 		
 			//If the current child is being displayed has a wish added update the 
 			//wish label to show the added wish
-//			if(child != null && addedWish.getChildID() == child.getID())
-//				displayLabel(child, addedWish.getWishNumber());
-			
+			if(child != null && addedWish.getChildID() == child.getID())
+				labelPanel.repaint();
 		}
 		else if(dbe.getSource() != this && dbe.getType().equals("UPDATED_CHILD_WISH"))
 		{
@@ -83,24 +79,26 @@ public class WishLabelViewer extends JDialog implements DatabaseListener
 			
 			//If the current child is being displayed has a wish added update the 
 			//wish label to show the added wish
-//			if(child != null && updatedWish.getChildID() == child.getID())	
-//				displayLabel(child, updatedWish.getWishNumber());
+			if(child != null && updatedWish.getChildID() == child.getID())
+				labelPanel.repaint();
 		}
 		else if(dbe.getType().equals("UPDATED_CHILD"))
 		{
 			ONCChild updatedChild = (ONCChild) dbe.getObject();
-//			if(child != null && child.getID() == updatedChild.getID())
-//			{
-//				//the age or gender of the child may have changed, update the label
-//				displayLabel(updatedChild, wn);
-//			}
+			if(child != null && child.getID() == updatedChild.getID())
+			{
+				//the age or gender of the child may have changed, update the label
+				labelPanel.repaint();
+			}
 		}
 	}
 	
 	private class WishLabelPanel extends JPanel
 	{
-		private static final int AVERY_LABEL_X_BARCODE_OFFSET = 0;
-		private static final int AVERY_LABEL_Y_BARCODE_OFFSET = 4;
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private static final double X_DISPLAY_SCALE_FACTOR = 1.6;
 		private static final double Y_DISPLAY_SCALE_FACTOR = 1.4;
 		
@@ -131,87 +129,8 @@ public class WishLabelViewer extends JDialog implements DatabaseListener
 		    SortWishObject swo = new SortWishObject(0, fam, child, cw);
 		    String[] line = swo.getWishLabel();
 		    
-		    AveryWishLabelPrinter awlp = new AveryWishLabelPrinter(null, null, 0);
+		    AveryWishLabelPrinter awlp = new AveryWishLabelPrinter();
 		    awlp.drawLabel(10, 20, line, lFont, img, g2d);
 		}
-/*		
-		void drawLabel(int x, int y, String[] line, Font[] lFont, Image img, Graphics2D g2d)
-		{
-			//draw either the season Icon or a bar code
-			if(GlobalVariables.getInstance().includeBarcodeOnLabels())
-				drawBarCode(line[4], x, y, g2d);	//draw the bar code on label
-			else
-				drawHolidayIcon(x, y, img, g2d);	//draw this seasons ONC icon on label
-  
-		    //Draw the label text, either 3 or 4 lines, depending on the wish base + detail length
-			g2d.setColor(Color.BLACK);
-		    g2d.setFont(lFont[0]);
-		    drawCenteredString(line[0], 120, x+50, y+5, g2d);	//Draw line 1
-		    
-		    g2d.setFont(lFont[1]);
-		    drawCenteredString(line[1], 120, x+50, y+20, g2d);	//Draw line 2
-		    
-		    if(line[3] == null)	//Only a 3 line label
-		    {
-		    	g2d.setFont(lFont[2]);
-		    	drawCenteredString(line[2], 120, x+50, y+35, g2d);	//Draw line 3
-		    }
-		    else	//A 4 line label
-		    {	    	
-		    	drawCenteredString(line[2], 120, x+50, y+35, g2d);	//Draw line 3	    	
-		    	g2d.setFont(lFont[2]);
-		    	drawCenteredString(line[3], 120, x+50, y+50, g2d);	//Draw line 4
-		    }
-		}
-		
-		private void drawHolidayIcon(int x, int y, Image img, Graphics2D g2d)
-		{
-			double scaleFactor = (72d / 300d) * 2;
-    	     
-		    // Now we perform our rendering 	       	    
-		    int destX1 = (int) (img.getWidth(null) * scaleFactor);
-		    int destY1 = (int) (img.getHeight(null) * scaleFactor);
-		    
-		    //Draw image scaled to fit image clip region on the label
-		    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		    g2d.drawImage(img, x, y, x+destX1, y+destY1, 0,0, img.getWidth(null),img.getHeight(null),null); 
-		}
-		
-		private void drawCenteredString(String s, int width, int XPos, int YPos, Graphics2D g2d)
-		{  
-	        int stringLen = (int) g2d.getFontMetrics().getStringBounds(s, g2d).getWidth();  
-	        int start = width/2 - stringLen/2; 
-	        g2d.drawString(s, start + XPos, YPos);  
-		}
-
-		private void drawBarCode(String code, int x, int y, Graphics2D g2d)
-		{
-			//create the bar code
-			
-			AbstractBarcodeBean bean;
-			if(gvs.getBarcodeCode() == Barcode.CODE128)
-				 bean = new Code128Bean();
-			else
-				bean = new UPCEBean();
-			
-			//get a temporary graphics context
-			Graphics2D tempg2d = (Graphics2D) g2d.create();
-			
-			//create the canvass
-			Java2DCanvasProvider cc = new Java2DCanvasProvider(tempg2d, 0);
-			tempg2d.translate(x + AVERY_LABEL_X_BARCODE_OFFSET, y + AVERY_LABEL_Y_BARCODE_OFFSET);
-//			tempg2d.translate(x, y);
-			
-			tempg2d.setRenderingHint( RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-//			tempg2d.scale(2.835, 2.835);	//scale from millimeters to points
-			tempg2d.scale(2.4, 2.4);	//scale from millimeters to points
-
-			//set the bean content
-			bean.generateBarcode(cc, code);
-			
-			//release the graphics context
-			tempg2d.dispose(); 
-		}
-*/		
 	}	
 }
