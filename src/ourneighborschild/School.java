cb6100856a467e8fd9def0d10e85d036733d1d61
oneilljw <johnwoneill1@gmail.com>
2018-07-27 10:12:06 -0400

    new class or updated region/school approach

---
+++ src/ourneighborschild/School.java
@@ -0,0 +1,20 @@
+package ourneighborschild;
+
+public class School
+{
+	private String code;
+	private Address address;
+	private String name;
+	
+	public School(String code, Address address, String name)
+	{
+		this.code = code;
+		this.address = address;
+		this.name = name;
+	}
+	
+	//getters
+	public String getCode() { return code; }
+	public Address getAddress() { return address; }
+	public String getName() { return name; }
+}
