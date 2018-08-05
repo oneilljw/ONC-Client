80c62470d3b757374916e4d60c37d5369c3eba61
oneilljw <johnwoneill1@gmail.com>
2018-08-05 11:42:06 -0400

    Website referral address error updates

--- src/ourneighborschild/AddressValidation.java
+++ src/ourneighborschild/AddressValidation.java
@@ -2,35 +2,30 @@ package ourneighborschild;
 
 public class AddressValidation 
 {
-	private boolean bAddressValid, bAddressIsSchool;
+	private boolean  bAddressIsSchool;
 	private int errorCode;
 	private String errMssg;
 		
-	public AddressValidation(boolean bAddressValid, boolean bAddressIsSchool, int errorCode)
+	public AddressValidation(boolean bAddressIsSchool, int errorCode)
 	{
-		this.bAddressValid = bAddressValid;
 		this.bAddressIsSchool = bAddressIsSchool;
 		this.errorCode = errorCode;
 		this.errMssg = "";
 	}
 	
-	public AddressValidation(boolean bAddressValid, boolean bAddressIsSchool, int errorCode, 
-							String errMssg)
+	public AddressValidation(boolean bAddressIsSchool, int errorCode, String errMssg)
 	{
-		this.bAddressValid = bAddressValid;
 		this.bAddressIsSchool = bAddressIsSchool;
 		this.errorCode = errorCode;
 		this.errMssg = errMssg;
 	}
 		
 	//getters
-	boolean isAddressValid() { return bAddressValid; }
 	boolean isAddressSchool() { return bAddressIsSchool; }
 	int getErrorCode() { return errorCode; }
 	String getErrorMessage() { return errMssg; }
 		
 	//setters
-	void setAddressValid(boolean tf) { this.bAddressValid = tf; }
 	void setAddressIsSchool(boolean tf) { this.bAddressIsSchool = tf; }
 	void setErrorCode(int ec) { this.errorCode = ec; }
 	void setErrorMessage(String errMssg) { this.errMssg = errMssg; }
