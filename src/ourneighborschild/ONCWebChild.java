7fdd43d97c3d67c85a36062202090b0efdb18303
johnoneill <johnoneill@johns-mbp>
2020-09-05 21:48:20 -0400

    Update for wish 1 being Age Appropriate

--- src/ourneighborschild/ONCWebChild.java
+++ src/ourneighborschild/ONCWebChild.java
@@ -29,7 +29,8 @@ public class ONCWebChild
 		this.gender = c.getChildGender();
 		this.sDOB = c.getChildDOBString("M/dd/yyyy");
 		this.wish0 = "";
-		this.wish1 = "";
+//		this.wish1 = "";
+		this.wish1 = "Age Appropriate Gift";	//Changed in 2020: Wish 2 requried to be "Age Appropriate Gift"
 //		this.wish2 = "Age Appropriate Gift";	//Changed in 2020: Only 2 Gifts allowed. 
 		this.wish2 = "No Wish 3 - Covid 19";
 		this.wish3 = "";
