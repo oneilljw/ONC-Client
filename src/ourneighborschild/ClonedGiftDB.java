3ee5e0406febe7129a2a7af933ad37ad1d3ed098
johnoneill <johnoneill@johns-mbp>
2020-10-29 22:59:52 -0400

    Update to rel v8.04

---
+++ src/ourneighborschild/ClonedGiftDB.java
@@ -0,0 +1,263 @@
+package ourneighborschild;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class ClonedGiftDB extends ONCDatabase
+{
+	private static ClonedGiftDB instance = null;
+	private ArrayList<ClonedGift> clonedGiftList;
+	
+	private ClonedGiftDB()
+	{	
+		super();
+		clonedGiftList = new ArrayList<ClonedGift>();
+	}
+	
+	public static ClonedGiftDB getInstance()
+	{
+		if(instance == null)
+			instance = new ClonedGiftDB();
+		
+		return instance;
+	}
+	
+	List<ClonedGift> getCurrentCloneGiftList()
+	{
+		List<ClonedGift> currClonedGiftList = new ArrayList<ClonedGift>();
+		for(ClonedGift cg : clonedGiftList)
+			if(cg.getNextID() == -1)	//cloned gift is last in linked list, there for is current
+				currClonedGiftList.add(cg);
+		
+		return currClonedGiftList;
+	}
+	
+	//adds a list of child gifts to the cloned gift data base.
+	String addClonesFromChildGiftList(Object source, List<ONCChildGift> cloneChildGiftRequestList)
+	{		
+		String cb = UserDB.getInstance().getUserLNFI();
+		
+		List<ClonedGift> reqAddClonedGiftList = new ArrayList<ClonedGift>();
+		
+		//create the list of added cloned gifts
+		for(ONCChildGift addGiftReq : cloneChildGiftRequestList)
+			reqAddClonedGiftList.add(new ClonedGift(-1, cb, addGiftReq, 3));
+		
+		//wrap the child gift list in a json array and send the add request to the server
+		Gson gson = new Gson();
+		Type listOfClonedGifts = new TypeToken<ArrayList<ClonedGift>>(){}.getType();
+		String response = null;
+		response = serverIF.sendRequest("POST<add_clonedgiftlist>" + gson.toJson(reqAddClonedGiftList, listOfClonedGifts));
+
+		//get the response from the server, validate it. Once validated, decompose the response json
+		//to a list of added gift json strings, convert them to ClonedGift objects and process the adds.
+		//set the return string to indicate the server add list request was successful
+		int addedCloneGiftCount = 0;
+		if(response != null && response.startsWith("ADDED_LIST_CLONED_GIFTS"))
+		{
+			Type responseListType = new TypeToken<ArrayList<String>>(){}.getType();
+			List<String> responseList = gson.fromJson(response.substring(23), responseListType);
+			
+			for(String giftResponse : responseList)
+			{
+				if(giftResponse.startsWith("ADDED_CLONED_GIFT"))
+				{	
+					ClonedGift addedGift = gson.fromJson(giftResponse.substring(17), ClonedGift.class);
+					processAddedClonedGift(source, addedGift);
+					addedCloneGiftCount++;
+				}
+			}
+		}
+		
+		String pluralOrNot = addedCloneGiftCount == 1 ? "Gift" : "Gifts";
+		return String.format("%d Cloned %s Created", addedCloneGiftCount, pluralOrNot);
+	}
+	
+	//updates a list of cloned gifts to the cloned gift data base.
+	String addClonedGiftList(Object source, List<ClonedGift> addGiftRequestList)
+	{	
+		List<ClonedGift> reqAddClonedGiftList = new ArrayList<ClonedGift>();
+		
+		//create the list of added cloned gifts
+		for(ClonedGift addGiftReq : addGiftRequestList)
+			reqAddClonedGiftList.add(new ClonedGift(UserDB.getInstance().getUserLNFI(), addGiftReq));
+		
+		//wrap the child gift list in a json array and send the add request to the server
+		Gson gson = new Gson();
+		Type listOfClonedGifts = new TypeToken<ArrayList<ClonedGift>>(){}.getType();
+		String response = null, returnResp = "ADD_FAILED";
+		response = serverIF.sendRequest("POST<add_clonedgiftlist>" + gson.toJson(reqAddClonedGiftList, listOfClonedGifts));
+
+		//get the response from the server, validate it. Once validated, decompose the response json
+		//to a list of added gift json strings, convert them to ClonedGift objects and process the adds.
+		//set the return string to indicate the server add list request was successful
+		if(response != null && response.startsWith("ADDED_LIST_CLONED_GIFTS"))
+		{
+			Type responseListType = new TypeToken<ArrayList<String>>(){}.getType();
+			List<String> responseList = gson.fromJson(response.substring(23), responseListType);
+			
+			for(String giftResponse : responseList)
+			{
+				if(giftResponse.startsWith("UPDATED_CLONED_GIFT"))
+				{	
+					ClonedGift updatedGift = gson.fromJson(giftResponse.substring(19), ClonedGift.class);
+					processUpdatedClonedGift(source, updatedGift);
+				}
+				else if(giftResponse.startsWith("ADDED_CLONED_GIFT"))
+				{	
+					ClonedGift addedGift = gson.fromJson(giftResponse.substring(17), ClonedGift.class);
+					processAddedClonedGift(source, addedGift);
+				}
+			}
+			
+			returnResp = "ADDED_GIFT_LIST";
+		}
+
+		return returnResp;
+	}
+	
+	void processUpdatedClonedGift(Object source, ClonedGift updatedClonedGift)
+	{
+		//store updated object in local data base
+		int index = 0;
+		while(index < clonedGiftList.size() && clonedGiftList.get(index).getID() != updatedClonedGift.getID())
+			index++;
+		
+		if(index < clonedGiftList.size())
+		{
+			replaceObject(index, updatedClonedGift);
+			
+			//Notify local user IFs that a change occurred
+			fireDataChanged(source, "UPDATED_CLONED_GIFT", updatedClonedGift);
+		}
+		
+	}
+	
+	void replaceObject(int index, ONCObject updatedObj)
+	{
+		ClonedGift updatedGift = (ClonedGift) updatedObj;
+		clonedGiftList.set(index,  updatedGift);
+	}
+	
+	ClonedGift processAddedClonedGift(Object source, ClonedGift addedGift)
+	{	
+		//add the new gift to the local data base
+		clonedGiftList.add(addedGift);
+		
+		//data bases have been updated, notify ui's of changes
+		fireDataChanged(source, "ADDED_CLONED_GIFT", addedGift);
+		
+		return addedGift;
+	}
+	
+	ClonedGift getClonedGift(int childID, int gn)
+	{
+		int index = clonedGiftList.size() -1;
+		while(index >= 0 && 
+				!(clonedGiftList.get(index).getChildID() == childID &&
+				   clonedGiftList.get(index).getGiftNumber() == gn &&
+				    clonedGiftList.get(index).getNextID() == -1))
+		{
+			index--;
+		}
+		
+		return index >= 0 ? clonedGiftList.get(index) : null;
+	}
+	
+	List<ClonedGift> getGiftHistory(int childID, int gn)
+	{
+		List<ClonedGift> cghList = new ArrayList<ClonedGift>();
+		Gson gson = new Gson();
+		
+		HistoryRequest req = new HistoryRequest(childID, gn);
+		String response = serverIF.sendRequest("GET<clonedgifthistory>"+ 
+											gson.toJson(req, HistoryRequest.class));
+		
+		if(response != null)
+		{
+			Type listtype = new TypeToken<ArrayList<ClonedGift>>(){}.getType();	
+			cghList = gson.fromJson(response, listtype);
+		}
+		
+		return cghList;
+	}
+	
+
+	ClonedGift getClonedGift(int id)
+	{
+		int index = 0;
+		while(index < clonedGiftList.size() && clonedGiftList.get(index).getID() != id)
+			index++;
+		
+		return index < clonedGiftList.size() ? clonedGiftList.get(index) : null;
+	}
+	
+	String importClonedGiftDatabase()
+	{
+		String response = "NO_CLONED_GIFTS";
+		
+		if(serverIF != null && serverIF.isConnected())
+		{		
+			Gson gson = new Gson();
+			Type listtype = new TypeToken<ArrayList<ClonedGift>>(){}.getType();
+			
+			response = serverIF.sendRequest("GET<clonedgifts>");
+			clonedGiftList = gson.fromJson(response, listtype);			
+
+			if(!response.startsWith("NO_CLONED_GIFTS"))
+			{
+				response =  "CLONED_GIFTS_LOADED";
+				fireDataChanged(this, "LOADED_CLONED_GIFTS", null);
+			}
+		}
+		
+		return response;
+	}
+	
+	@Override
+	public void dataChanged(ServerEvent ue)
+	{
+		if(ue.getType().equals("ADDED_CLONED_GIFT"))
+		{
+			Gson gson = new Gson();
+			processAddedClonedGift(this, gson.fromJson(ue.getJson(), ClonedGift.class));
+		}
+		else if(ue.getType().equals("UPDATED_CLONED_GIFT"))
+		{
+			Gson gson = new Gson();
+			processUpdatedClonedGift(this, gson.fromJson(ue.getJson(), ClonedGift.class));
+		}
+		else if(ue.getType().equals("ADDED_LIST_CLONED_GIFTS"))
+		{
+			Gson gson = new Gson();
+			Type responseListType = new TypeToken<ArrayList<String>>(){}.getType();
+			List<String> addedGiftListString = gson.fromJson(ue.getJson(), responseListType);
+			for(String giftResponse : addedGiftListString)
+			{
+				if(giftResponse.startsWith("UPDATED_CLONED_GIFT"))
+				{	
+					ClonedGift updatedGift = gson.fromJson(giftResponse.substring(19), ClonedGift.class);
+					processUpdatedClonedGift(this, updatedGift);
+				}
+				else if(giftResponse.startsWith("ADDED_CLONED_GIFT"))
+				{	
+					ClonedGift addedGift = gson.fromJson(giftResponse.substring(17), ClonedGift.class);
+					processAddedClonedGift(this, addedGift);
+				}
+			}
+		}
+	}
+
+	@Override
+	String update(Object source, ONCObject entity)
+	{
+		//Cloned gifts are never updated by a user, an update only occurs to modify the
+		//the linked list chain
+		return null;
+	}
+
+}
---
+++ src/ourneighborschild/SortClonedGiftObject.java
@@ -0,0 +1,175 @@
+package ourneighborschild;
+
+import java.text.SimpleDateFormat;
+
+public class SortClonedGiftObject extends ONCObject
+{
+	private static final int MAX_LABEL_LINE_LENGTH = 26;
+	private ONCFamily	 soFamily;
+	private ONCChild	 soChild;
+	private ClonedGift soClonedGift;
+	
+	PartnerDB partnerDB;
+	GiftCatalogDB cat;
+	RegionDB regionDB;
+	DNSCodeDB dnsCodeDB;
+	
+	String[] indicator = {"", "*", "#"};
+	
+	public SortClonedGiftObject(int itemID, ONCFamily fam, ONCChild c, ClonedGift cg) 
+	{
+		super(itemID);
+		this.soFamily = fam;
+		this.soChild = c;
+		this.soClonedGift = cg;
+		
+		partnerDB = PartnerDB.getInstance();
+		cat = GiftCatalogDB.getInstance();
+		regionDB = RegionDB.getInstance();
+		dnsCodeDB = DNSCodeDB.getInstance();
+	}
+	
+	//getters
+	ONCFamily getFamily() { return soFamily; }
+	ONCChild getChild() { return soChild; }
+	ClonedGift getClonedGift() { return soClonedGift; }
+	
+	public String[] getExportRow()
+	{
+		SimpleDateFormat dob = new SimpleDateFormat("MM-dd-yyyy");
+//		String dateChanged = dob.format(soClonedGift.getDateChanged().getTime());
+		String dateChanged = "FIX DATE CHANGE";
+		
+		ONCGift wish = cat.getGiftByID(soClonedGift.getGiftID());
+		String wishName = wish == null ? "None" : wish.getName();
+		
+		ONCPartner partner = partnerDB.getPartnerByID(soClonedGift.getPartnerID());
+		String partnerName = partner == null ? "" : partner.getLastName();
+		
+		String[] exportRow = {soFamily.getONCNum(),
+								dnsCodeDB.getDNSCode(soFamily.getDNSCode()).toString(),
+								regionDB.getSchoolName(soFamily.getSchoolCode()),
+								soChild.getChildGender(),
+								soChild.getChildAge(),
+								soChild.getChildDOBString("MM-dd-yyyy"), 
+								indicator[soClonedGift.getIndicator()],
+								wishName,
+								soClonedGift.getDetail(),
+								soClonedGift.getGiftStatus().toString(),
+								partnerName,
+								soClonedGift.getChangedBy(),
+								dateChanged};
+		return exportRow;
+	}
+	
+	String[] getGiftLabel()
+	{	
+		GlobalVariablesDB gvs = GlobalVariablesDB.getInstance();
+		GiftCatalogDB cat = GiftCatalogDB.getInstance();
+		
+		String[] line = new String[5];
+		SimpleDateFormat sYear = new SimpleDateFormat("yyyy");
+		
+//		String[] indicator = {"", "*", "#"};
+		
+		//Changed when adding bar code to labels			
+		line[0] = soChild.getChildAge() + " " + soChild.getChildGender();
+		
+		if(soClonedGift.getDetail().isEmpty())
+		{
+			ONCGift gift = cat.getGiftByID(soClonedGift.getGiftID());
+			String giftName = gift == null ? "None" : gift.getName();
+			
+			line[1] = giftName + "- ";
+			line[2] = "ONC " + sYear.format(gvs.getSeasonStartDate()) + 
+						" |  Family # " + soFamily.getONCNum();
+			line[3] = null;
+		}	
+		else
+		{
+			ONCGift catGift = cat.getGiftByID(soClonedGift.getGiftID());
+			String giftName = catGift == null ? "None" : catGift.getName();
+			
+			String gift = giftName.equals("-") ? soClonedGift.getDetail() : 
+				giftName + "- " + soClonedGift.getDetail();
+	
+			//does it fit on one line?
+			if(gift.length() <= MAX_LABEL_LINE_LENGTH)
+			{
+				line[1] = gift.trim();
+			}
+			else	//split into two lines
+			{
+				int index = MAX_LABEL_LINE_LENGTH;
+				while(index > 0 && gift.charAt(index) != ' ')	//find the line break
+					index--;
+	
+				line[1] = gift.substring(0, index);
+				line[2] = gift.substring(index);
+				if(line[2].length() > MAX_LABEL_LINE_LENGTH)
+					line[2] = gift.substring(index, index + MAX_LABEL_LINE_LENGTH);
+			}
+
+			//If the gift required two lines make the ONC Year line 4
+			//else make the ONC Year line 3
+			if(line[2] != null)
+			{
+				line[3] = "ONC " + sYear.format(gvs.getSeasonStartDate()) + 
+							" |  Family # " + soFamily.getONCNum();
+			}
+			else
+			{			
+				line[2] = "ONC " + sYear.format(gvs.getSeasonStartDate()) + 
+							" |  Family # " + soFamily.getONCNum();
+				line[3] = null;
+			}
+		}
+		
+		//generate leading zero padded bar code string for child # & gift #
+		if(soClonedGift.getChildID() < 10)
+			line[4] = "00000" + Integer.toString(soClonedGift.getChildID());
+		else if(soClonedGift.getChildID() < 100)
+			line[4] = "0000" + Integer.toString(soClonedGift.getChildID());
+		else if(soClonedGift.getChildID() < 1000)
+			line[4] = "000" + Integer.toString(soClonedGift.getChildID());
+		else if(soClonedGift.getChildID() < 10000)
+			line[4] = "00" + Integer.toString(soClonedGift.getChildID());
+		else if(soClonedGift.getChildID() < 100000)
+			line[4] = "0" + Integer.toString(soClonedGift.getChildID());
+		else if(soClonedGift.getChildID() < 1000000)
+			line[4] =  Integer.toString(soClonedGift.getChildID());
+		else
+			line[4] = Integer.toString(soClonedGift.getID());
+		
+		line[4] = line[4] + Integer.toString(soClonedGift.getGiftNumber());	
+	
+		return line;
+	}
+	
+	String getGiftPlusDetail()
+	{
+		ONCGift catGift = cat.getGiftByID(soClonedGift.getGiftID());
+		String giftName = catGift == null ? "None" : catGift.getName();
+		
+		return giftName.equals("-") ? soClonedGift.getDetail() : 
+			giftName + "- " + soClonedGift.getDetail();
+	}
+	
+	//determine if two SortClonedGiftObjects match. If the clonedGift cn & gn match, the childGifts match,
+	//regardless of the actual content of the wish. Override the method in the base ONCObject class
+	@Override
+	public boolean matches(ONCObject otherObj)
+	{
+		if(otherObj != null && otherObj.getClass() == SortClonedGiftObject.class)
+		{
+			SortClonedGiftObject otherSO = (SortClonedGiftObject) otherObj;
+				
+			return otherSO.soFamily != null && otherSO.soFamily.getID() == soFamily.getID() &&
+					otherSO.soChild != null && otherSO.soChild.getID() == soChild.getID() &&
+					otherSO.soClonedGift != null && otherSO.soClonedGift.getChildID() == soClonedGift.getChildID() &&
+					otherSO.soClonedGift.getGiftNumber() == soClonedGift.getGiftNumber();		
+		}
+		else
+			return false;
+	}
+}
